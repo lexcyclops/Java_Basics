3697526486de04419b74821ed38fb51266254e58
Lekshman Sujesh  R <lexcyclops@gmail.com>
2026-08-09 20:08:31 +0530

    Added a code About Class and Objects

---
+++ CLASS.java
@@ -0,0 +1,17 @@
+public class CLASS {
+
+    public static void main(String a[]){
+        classname s1=new classname();
+        classname s2=new classname();
+
+        s1.a=20;
+        s2.a=50;
+        s1.b=100;
+        s2.b=200;
+
+        s1.display();
+        s2.display();
+
+    }
+    
+}
---
+++ classname.java
@@ -0,0 +1,7 @@
+public class classname {
+    int a;
+    int b;
+    void display(){
+        System.out.print("   a:"+a+"   b:"+b);
+    }
+}
