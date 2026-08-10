634f366c7dfbc252be60869fe1960a2566fa587c
Lekshman Sujesh  R <lexcyclops@gmail.com>
2026-08-10 23:40:31 +0530

    Added a topic absed on method delegation

---
+++ Finalclass.java
@@ -0,0 +1,6 @@
+public class Finalclass {
+
+    void displayFinalclass(){
+        System.out.println("This makes coding interesting");
+    }
+}
---
+++ Second.java
@@ -0,0 +1,8 @@
+public class Second {
+    
+    void displaySecond(){
+        Finalclass f=new Finalclass();
+        f.displayFinalclass();
+    }
+
+}
