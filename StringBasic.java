5f75bc3b5f10e57863c91605b64539edee211a87
Lekshman Sujesh  R <lexcyclops@gmail.com>
2026-08-11 22:30:11 +0530

    Add Java String length and toUpperCase method

---
+++ StringBasic.java
@@ -0,0 +1,9 @@
+public class StringBasic {
+    public static void main(String[] args) {
+        String name = "Java Programmer";
+        System.out.println("Length: " + name.length());
+        System.out.println("Uppercase: " + name.toUpperCase());
+    }
+} 
+    
+
