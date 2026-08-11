7e9f3c462228d8e3d9939ad216111dcc500cb6cd
Lekshman Sujesh  R <lexcyclops@gmail.com>
2026-08-11 22:31:58 +0530

    Add Java program to reverse a String using for loop

---
+++ StringReverse.java
@@ -0,0 +1,12 @@
+public class StringReverse{
+    public static void main(String[] args) {
+        String str = "JAVA";
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        System.out.println("Reversed: " + rev);
+    }
+} 
+    
+
