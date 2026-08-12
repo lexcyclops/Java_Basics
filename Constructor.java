b1e6a5ed283e75e019cf6a8d9917fce5ec534c95
Lekshman Sujesh  R <lexcyclops@gmail.com>
2026-08-12 23:48:39 +0530

    Created a code based on the  topic contructors in Java

---
+++ Constructor.java
@@ -0,0 +1,9 @@
+public class Constructor {
+    public static void main(String [] args){
+        Sample s=new Sample();
+        Constructor h=new Constructor();
+    }
+    Constructor(){
+        System.out.println("Hello");
+    }
+}
---
+++ Sample.java
@@ -0,0 +1,5 @@
+public class Sample {
+    Sample(){
+        System.out.println("Introduction to constructors");
+    }
+}
