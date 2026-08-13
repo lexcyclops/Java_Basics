be7a2e13a2f4c2d2b56009b7558abb22543e189c
Lekshman Sujesh  R <lexcyclops@gmail.com>
2026-08-13 23:25:07 +0530

    Added a code based on the topic overriding in polymorphism

---
+++ OrCar.java
@@ -0,0 +1,6 @@
+public class OrCar {
+    void display(){
+         System.out.println("I love BMW m3");
+    }
+   
+}
---
+++ Target.java
@@ -0,0 +1,12 @@
+public class Target extends OrCar{
+    void display(){
+        System.out.println("I like to buy a pagani than a BMV");
+
+    }
+
+    public static void main(String[] args){
+        Target s1=new Target();
+
+        s1.display();
+    }
+}
