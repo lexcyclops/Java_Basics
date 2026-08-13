c22b185f40c6c08469e743ebbecd8427baae8cd9
Lekshman Sujesh  R <lexcyclops@gmail.com>
2026-08-13 21:14:33 +0530

    Added a code which explain the topic this keyword

---
+++ keyword_sample.java
@@ -0,0 +1,14 @@
+public class keyword_sample {
+    int rollNo;
+    String name;
+    
+    keyword_sample(int rollNo, String name){
+        
+        this.rollNo = rollNo;
+        this.name = name;
+
+    }
+    void display(){
+        System.out.println("Roll no : "+rollNo +"\n Name: "+name);
+    }
+}
---
+++ keyword_test.java
@@ -0,0 +1,11 @@
+public class keyword_test {
+    public static void main(String [] args){
+
+        keyword_sample s1=new keyword_sample(25,"Lex");
+        keyword_sample s2=new keyword_sample(34,"ixia");
+
+        s1.display();
+        s2.display();
+        
+    }
+}
