5d1e7c99cc99dfe16569cc9ae648b7b3714ef173
Lekshman Sujesh  R <lexcyclops@gmail.com>
2026-08-13 22:57:29 +0530

    Added a realworld example code about the overloading in polymorphism

---
+++ calculaminput.java
@@ -0,0 +1,9 @@
+public class calculaminput {
+    public static void main (String [] args){
+        calculam s1=new calculam();
+
+        System.out.println("Sum of two numbers: "+ s1.add( 20,54));
+        System.out.println("Sum of three numbers: "+ s1.add(32,67,62));
+        System.out.println("Sum of two double variables: "+s1.add(53.73,54.32));
+    }
+}
