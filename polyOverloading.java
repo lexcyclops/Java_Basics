ae3ec6d5739c88889c2bcad6b9562948828ebad4
Lekshman Sujesh  R <lexcyclops@gmail.com>
2026-08-13 22:23:09 +0530

    Added a code based on the topic polymorphism method over loading

---
+++ polyOverloading.java
@@ -0,0 +1,7 @@
+public class polyOverloading {
+    public static void main(String [] args){
+        polySum s1=new polySum();
+        s1.display();
+        s1.display(34);
+    }
+}
---
+++ polySum.java
@@ -0,0 +1,9 @@
+public class polySum {
+    void display(){
+        System.out.println("This is the fist method");
+
+    }
+    void display(int a){
+        System.out.println("The value you have given is "+ a);
+    }
+}
