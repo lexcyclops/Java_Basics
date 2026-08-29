520a14743398b270b1b3efe1342fa1a9a839877d
Lekshman Sujesh  R <lexcyclops@gmail.com>
2026-08-29 20:47:50 +0530

    Added a code based on the topic exception (try,catch,finally)

---
+++ Exeption.java
@@ -0,0 +1,20 @@
+public class Exeption {
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            int b = 0;
+
+            int result = a / b;
+            System.out.println("Result: " + result);
+
+        } catch (ArithmeticException e) {
+    
+            System.out.println("Error: A number can't be devided by 0!");
+
+        } finally {
+
+            System.out.println("Finally Block: This line always print.");
+        }
+    }
+}
+
