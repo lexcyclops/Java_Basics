a5702a0e31babd0624c7306528f7077e6344b93f
Lekshman Sujesh  R <lexcyclops@gmail.com>
2026-08-29 20:11:34 +0530

    Added a new folder creating package and using the package

---
+++ src/test1/SamplePackage1.java
@@ -0,0 +1,6 @@
+package src.test1;
+
+public class SamplePackage1 {
+
+ 
+}
---
+++ src/test2/SamplePackage2.java
@@ -0,0 +1,13 @@
+package src.test2;
+
+import java.util.Scanner;
+
+import src.test1.SamplePackage1;
+
+public class SamplePackage2 {
+
+    SamplePackage2(){
+        SamplePackage1 h=new SamplePackage1();
+        Scanner s=new Scanner(System.in);
+    }
+}
