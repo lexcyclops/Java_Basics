a893200820b89adde9e2a4581a7d530a0ba581db
Lekshman Sujesh  R <lexcyclops@gmail.com>
2026-08-29 20:23:11 +0530

    this also include

--- src/test2/SamplePackage2.java
+++ src/test2/SamplePackage2.java
@@ -1,14 +1,11 @@
 package src.test2;
 
-import java.util.Scanner;
-
 import src.test1.SamplePackage1;
 
 public class SamplePackage2 {
 
     SamplePackage2(){
         SamplePackage1 h=new SamplePackage1();
-        Scanner s=new Scanner(System.in);
 
         h.display();
         SampleTest2 q=new SampleTest2();
